53fd6e6b188b785a924724b22f578c346b004d41
Joao Carvalho <9712239j@gmail.com>
2025-09-07 10:14:10 -0300

    [FEATURE] Reorganizing modules

--- src/app/features/clients/clients.module.ts
+++ src/app/features/clients/clients.module.ts
@@ -1,22 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { ClientsRoutingModule } from './clients-routing.module';
 import { ClientsListComponent } from './pages/clients-list/clients-list.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { CreateClientComponent } from './pages/create-client/create-client.component';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatDialogModule } from '@angular/material/dialog';
 import { ClientDeleteConfirmationModal } from './components/client-delete-confirmation-modal/client-delete-confirmation-modal.component';
 import { UpdateClientComponent } from './pages/update-client/update-client.component';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { NgxMaskDirective, provideNgxMask, NgxMaskPipe } from 'ngx-mask';
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
@@ -26,23 +15,6 @@ import { SharedModule } from '../../shared/shared.module';
     ClientDeleteConfirmationModal,
     UpdateClientComponent,
   ],
-  imports: [
-    CommonModule,
-    ClientsRoutingModule,
-    MatTableModule,
-    MatButtonModule,
-    MatIconModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatCardModule,
-    MatGridListModule,
-    MatDialogModule,
-    MatProgressSpinnerModule,
-    NgxMaskDirective,
-    NgxMaskPipe,
-    SharedModule
-  ],
-  providers: [provideNgxMask()],
+  imports: [CommonModule, ClientsRoutingModule, SharedModule],
 })
 export class ClientsModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,51 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [LoadingSpinnerComponent],
-  imports: [CommonModule, MatProgressSpinnerModule],
-  exports: [LoadingSpinnerComponent, MatProgressSpinnerModule],
+  imports: [
+    CommonModule,
+    MatProgressSpinnerModule,
+    NgxMaskPipe,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatGridListModule,
+    MatDialogModule,
+    MatProgressSpinnerModule,
+    NgxMaskDirective,
+  ],
+  exports: [
+    LoadingSpinnerComponent,
+    MatProgressSpinnerModule,
+    NgxMaskPipe,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatGridListModule,
+    MatDialogModule,
+    MatProgressSpinnerModule,
+    NgxMaskDirective,
+  ],
+  providers: [provideNgxMask()],
 })
 export class SharedModule {}
