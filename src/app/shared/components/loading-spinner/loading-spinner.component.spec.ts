f0ca943272968da39f8210d7d8498098c45c4cb8
Joao Carvalho <9712239j@gmail.com>
2025-09-08 01:35:18 -0300

    [FEATURE] Adding loading spinner component test

--- src/app/shared/components/loading-spinner/loading-spinner.component.spec.ts
+++ src/app/shared/components/loading-spinner/loading-spinner.component.spec.ts
@@ -1,16 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoadingSpinnerComponent } from './loading-spinner.component';
+import { By } from '@angular/platform-browser';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
-describe('LoadingDialogComponent', () => {
+describe('LoadingSpinnerComponent', () => {
   let component: LoadingSpinnerComponent;
   let fixture: ComponentFixture<LoadingSpinnerComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LoadingSpinnerComponent]
-    })
-    .compileComponents();
+      declarations: [LoadingSpinnerComponent],
+      imports: [MatProgressSpinnerModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoadingSpinnerComponent);
     component = fixture.componentInstance;
@@ -19,5 +21,32 @@ describe('LoadingDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should not display the spinner when isLoading is false', () => {
+    const spinnerElement = fixture.debugElement.query(By.css('mat-spinner'));
+    expect(spinnerElement).toBeNull();
+  });
+
+  it('should display the spinner when isLoading is true', () => {
+    component.isLoading = true;
+    fixture.detectChanges();
+
+    const spinnerElement = fixture.debugElement.query(By.css('mat-spinner'));
+    expect(spinnerElement).not.toBeNull();
+  });
+
+  it('should hide the spinner when isLoading changes from true to false', () => {
+    component.isLoading = true;
+    fixture.detectChanges();
+
+    let spinnerElement = fixture.debugElement.query(By.css('mat-spinner'));
+    expect(spinnerElement).not.toBeNull();
+
+    component.isLoading = false;
+    fixture.detectChanges();
+
+    spinnerElement = fixture.debugElement.query(By.css('mat-spinner'));
+    expect(spinnerElement).toBeNull();
   });
 });
